d085e50a6646d682460c02f33f6681a93cf8c73c
Andrey Novikov <novikov@gmail.com>
2015-11-20 15:20:50 +0300

    Utils

---
+++ app/src/main/java/mobi/maptrek/util/FileUtils.java
@@ -0,0 +1,53 @@
+package mobi.maptrek.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileUtils
+{
+	public static String unusable = "*+~|<>!?\\/:";
+
+	/**
+	 * Replace illegal characters in a filename with "_" Illegal characters: : \
+	 * / * ? | < >
+	 * 
+	 * @param name Proposed file name
+	 * @return sanitized string
+	 */
+	public static String sanitizeFilename(String name)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < name.length(); i++)
+		{
+			if (unusable.indexOf(name.charAt(i)) > -1)
+				sb.append("_");
+			else
+				sb.append(name.charAt(i));
+		}
+		return sb.toString();
+	}
+
+	public static void copyFile(File from, File to) throws IOException
+	{
+		//create output directory if it doesn't exist
+		//noinspection ResultOfMethodCallIgnored
+		to.getParentFile().mkdirs();
+
+		InputStream in = new FileInputStream(from);
+		OutputStream out = new FileOutputStream(to);
+
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1)
+			out.write(buffer, 0, read);
+		in.close();
+
+		// write the output file
+		out.flush();
+		out.close();
+	}
+}
---
+++ app/src/main/java/mobi/maptrek/util/MonitoredInputStream.java
@@ -0,0 +1,108 @@
+package mobi.maptrek.util;
+
+import android.support.annotation.NonNull;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+/**
+ * A class that monitors the read progress of an input stream.
+ *
+ * @author Hermia Yeung "Sheepy"
+ * @since 2012-04-05 18:42
+ */
+public class MonitoredInputStream extends FilterInputStream {
+    private volatile long mMark = 0;
+    private volatile long mLastTriggeredLocation = 0;
+    private volatile long mLocation = 0;
+    private final int mThreshold;
+    private final List<ChangeListener> listeners = new ArrayList<>(1);
+
+
+    /**
+     * Creates a MonitoredInputStream over an underlying input stream.
+     *
+     * @param in        Underlying input stream, should be non-null because of no public setter
+     * @param threshold Min. position change (in byte) to trigger change event.
+     */
+    public MonitoredInputStream(InputStream in, int threshold) {
+        super(in);
+        this.mThreshold = threshold;
+    }
+
+    /**
+     * Creates a MonitoredInputStream over an underlying input stream.
+     * Default threshold is 16KB, small threshold may impact performance on larger streams.
+     *
+     * @param in Underlying input stream, should be non-null because of no public setter
+     */
+    public MonitoredInputStream(InputStream in) {
+        super(in);
+        this.mThreshold = 1024 * 16;
+    }
+
+    public void addChangeListener(ChangeListener l) {
+        if (!listeners.contains(l)) listeners.add(l);
+    }
+
+    public void removeChangeListener(ChangeListener l) {
+        listeners.remove(l);
+    }
+
+    public long getProgress() {
+        return mLocation;
+    }
+
+    protected void triggerChanged(final long location) {
+        if (mThreshold > 0 && Math.abs(location - mLastTriggeredLocation) < mThreshold) return;
+        mLastTriggeredLocation = location;
+        if (listeners.size() <= 0) return;
+        try {
+            for (ChangeListener l : listeners) l.stateChanged(location);
+        } catch (ConcurrentModificationException e) {
+            triggerChanged(location);  // List changed? Let's re-try.
+        }
+    }
+
+
+    @Override
+    public int read() throws IOException {
+        final int i = super.read();
+        if (i != -1) triggerChanged(mLocation++);
+        return i;
+    }
+
+    @Override
+    public int read(@NonNull byte[] b, int off, int len) throws IOException {
+        final int i = super.read(b, off, len);
+        if (i > 0) triggerChanged(mLocation += i);
+        return i;
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        final long i = super.skip(n);
+        if (i > 0) triggerChanged(mLocation += i);
+        return i;
+    }
+
+    @Override
+    public void mark(int readlimit) {
+        super.mark(readlimit);
+        mMark = mLocation;
+    }
+
+    @Override
+    public void reset() throws IOException {
+        super.reset();
+        if (mLocation != mMark) triggerChanged(mLocation = mMark);
+    }
+
+    public interface ChangeListener {
+        void stateChanged(long location);
+    }
+}
