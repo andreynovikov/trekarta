e9812e6f143640630b5d067115bd0ab60b9c7f36
Andrey Novikov <novikov@gmail.com>
2016-07-19 17:03:05 +0300

    Location marker position simplification

--- app/src/main/java/mobi/maptrek/layers/LocationOverlay.java
+++ app/src/main/java/mobi/maptrek/layers/LocationOverlay.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2013 Ahmad Saleem
  * Copyright 2013 Hannes Janetzek
+ * Copyright 2016 Andrey Novikov
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
@@ -17,7 +18,6 @@
 package mobi.maptrek.layers;
 
 import android.os.SystemClock;
-import android.util.Log;
 
 import org.oscim.backend.GL;
 import org.oscim.core.Box;
@@ -37,278 +37,203 @@ import org.oscim.utils.math.Interpolation;
 import static org.oscim.backend.GLAdapter.gl;
 
 public class LocationOverlay extends Layer {
-	private final int SHOW_ACCURACY_ZOOM = 18;
+    private final int SHOW_ACCURACY_ZOOM = 18;
 
-	private final Point mLocation = new Point();
+    private final Point mLocation = new Point();
     private float mBearing;
-	private double mRadius;
-	private double mLocationOffset;
-    private float mAveragedBearing;
-    private boolean mTrackMode;
+    private double mRadius;
 
     public LocationOverlay(Map map) {
-		super(map);
-		mRenderer = new LocationIndicator();
+        super(map);
+        mRenderer = new LocationIndicator();
         setEnabled(false);
-	}
+    }
 
-	public void setPosition(double latitude, double longitude, float bearing, float accuracy) {
-		mLocation.x = MercatorProjection.longitudeToX(longitude);
-		mLocation.y = MercatorProjection.latitudeToY(latitude);
+    public void setPosition(double latitude, double longitude, float bearing, float accuracy) {
+        mLocation.x = MercatorProjection.longitudeToX(longitude);
+        mLocation.y = MercatorProjection.latitudeToY(latitude);
         mBearing = bearing;
-		mRadius = accuracy / MercatorProjection.groundResolution(latitude, 1);
-		((LocationIndicator) mRenderer).animate(true);
-	}
-
-	public Point getPosition() {
-		return new Point(mLocation.x, mLocation.y);
-	}
-
-	@Override
-	public void setEnabled(boolean enabled) {
-		if (enabled == isEnabled())
-			return;
-
-		super.setEnabled(enabled);
-
-		//if (!enabled)
-			((LocationIndicator) mRenderer).animate(enabled);
-	}
-
-    public void setPinned(boolean pinned) {
-        //((LocationIndicator) mRenderer).setPinned(pinned);
+        mRadius = accuracy / MercatorProjection.groundResolution(latitude, 1);
+        ((LocationIndicator) mRenderer).animate(true);
     }
 
-    public boolean isPinned() {
-        return ((LocationIndicator) mRenderer).mPinned;
+    public Point getPosition() {
+        return new Point(mLocation.x, mLocation.y);
     }
 
-	public void setLocationOffset(double offset, float averagedBearing, boolean trackMode) {
-		mLocationOffset = offset;
-        mAveragedBearing = averagedBearing;
-        mTrackMode = trackMode;
-	}
+    @Override
+    public void setEnabled(boolean enabled) {
+        if (enabled == isEnabled())
+            return;
+
+        super.setEnabled(enabled);
+
+        //if (!enabled)
+        ((LocationIndicator) mRenderer).animate(enabled);
+    }
 
     public class LocationIndicator extends LayerRenderer {
-		private int mShaderProgram;
-		private int hVertexPosition;
-		private int hMatrixPosition;
-		private int hScale;
-		private int hPhase;
-		private int hDirection;
-		private int hType;
+        private int mShaderProgram;
+        private int hVertexPosition;
+        private int hMatrixPosition;
+        private int hScale;
+        private int hPhase;
+        private int hDirection;
+        private int hType;
 
-		public final static float POINTER_SIZE = 100;
+        public final static float POINTER_SIZE = 100;
 
-		private final static long ANIM_RATE = 50;
-		private final static long INTERVAL = 8000;
+        private final static long ANIM_RATE = 50;
+        private final static long INTERVAL = 8000;
 
-		private final Point mIndicatorPosition = new Point();
+        private final Point mIndicatorPosition = new Point();
 
-		private final Point mScreenPoint = new Point();
-		private final Box mBBox = new Box();
+        private final Box mBBox = new Box();
 
-		private boolean mInitialized;
+        private boolean mInitialized;
 
-		private boolean mLocationIsVisible;
+        private boolean mLocationIsVisible;
 
-		private boolean mRunAnim;
-		private long mAnimStart;
+        private boolean mRunAnim;
+        private long mAnimStart;
 
-        private boolean mPinned = false;
-		private boolean mReanimated = false;
+        private boolean mReanimated = false;
 
-		public LocationIndicator() {
-			super();
-		}
-
-        private void setPinned(boolean pinned) {
-            mLocationIsVisible = true;
-            mPinned = pinned;
+        public LocationIndicator() {
+            super();
         }
 
-		private void animate(boolean enable) {
-			if (mRunAnim == enable)
-				return;
+        private void animate(boolean enable) {
+            if (mRunAnim == enable)
+                return;
 
-			mReanimated = true;
-			mRunAnim = enable;
-			if (!enable)
-				return;
+            mReanimated = true;
+            mRunAnim = enable;
+            if (!enable)
+                return;
 
-			final Runnable action = new Runnable() {
-				private long lastRun;
+            final Runnable action = new Runnable() {
+                private long lastRun;
 
-				@Override
-				public void run() {
-					if (!mRunAnim)
-						return;
+                @Override
+                public void run() {
+                    if (!mRunAnim)
+                        return;
 
-					long diff = SystemClock.elapsedRealtime() - lastRun;
-					mMap.postDelayed(this, Math.min(ANIM_RATE, diff));
-					mMap.render();
-				}
-			};
+                    long diff = SystemClock.elapsedRealtime() - lastRun;
+                    mMap.postDelayed(this, Math.min(ANIM_RATE, diff));
+                    mMap.render();
+                }
+            };
 
-			mAnimStart = SystemClock.elapsedRealtime();
-			mMap.postDelayed(action, ANIM_RATE);
-		}
+            mAnimStart = SystemClock.elapsedRealtime();
+            mMap.postDelayed(action, ANIM_RATE);
+        }
 
-		private float animPhase() {
-			return (float) ((MapRenderer.frametime - mAnimStart) % INTERVAL) / INTERVAL;
-		}
+        private float animPhase() {
+            return (float) ((MapRenderer.frametime - mAnimStart) % INTERVAL) / INTERVAL;
+        }
 
-		@Override
-		public void update(GLViewport v) {
-			if (!mInitialized) {
-				init();
-				mInitialized = true;
-			}
+        @Override
+        public void update(GLViewport v) {
+            if (!mInitialized) {
+                init();
+                mInitialized = true;
+            }
 
-			if (!isEnabled()) {
-				setReady(false);
-				return;
-			}
-
-			if (!v.changed() && ! mReanimated && !isReady())
-				return;
-
-			setReady(true);
-
-            if (mPinned) {
-                double bearing = mTrackMode ? v.pos.bearing : mAveragedBearing;
-				double rad = Math.toRadians(bearing);
-				double dx = mLocationOffset * Math.sin(rad);
-				double dy = mLocationOffset * Math.cos(rad);
-				mIndicatorPosition.x = v.pos.x - dx;
-                mIndicatorPosition.y = v.pos.y + dy;
+            if (!isEnabled()) {
+                setReady(false);
                 return;
             }
 
-			int width = mMap.getWidth();
-			int height = mMap.getHeight();
+            if (!v.changed() && !mReanimated && !isReady())
+                return;
 
-			// clamp location to a position that can be
-			// safely translated to screen coordinates
-			v.getBBox(mBBox, 0);
+            setReady(true);
 
-			double x = mLocation.x;
-			double y = mLocation.y;
+            // clamp location to a position that can be
+            // safely translated to screen coordinates
+            v.getBBox(mBBox, 0);
 
-			if (!mBBox.contains(mLocation)) {
-				x = FastMath.clamp(x, mBBox.xmin, mBBox.xmax);
-				y = FastMath.clamp(y, mBBox.ymin, mBBox.ymax);
-			}
+            mLocationIsVisible = mBBox.contains(mLocation);
+            mIndicatorPosition.x = FastMath.clamp(mLocation.x, mBBox.xmin, mBBox.xmax);
+            mIndicatorPosition.y = FastMath.clamp(mLocation.y, mBBox.ymin, mBBox.ymax);
+        }
 
-			// get position of Location in pixel relative to
-			// screen center
-			v.toScreenPoint(x, y, mScreenPoint);
+        @Override
+        public void render(GLViewport v) {
+            GLState.useProgram(mShaderProgram);
+            GLState.blend(true);
+            GLState.test(false, false);
 
-			x = mScreenPoint.x;// + width / 2;
-			y = mScreenPoint.y;// + height / 2;
-            //Log.e("LO", x + ":" + y);
+            GLState.enableVertexArrays(hVertexPosition, -1);
+            MapRenderer.bindQuadVertexVBO(hVertexPosition);
 
-			// clip position to screen boundaries
-			int visible = 0;
-            double w2 = width / 2;
-            double h2 = height / 2;
+            float radius = POINTER_SIZE;
 
-			if (x > w2 - 5)
-				x = w2;
-			else if (x < -w2 + 5)
-				x = -w2;
-			else
-				visible++;
-
-			if (y > h2 - 5)
-				y = h2;
-			else if (y < -h2 + 5)
-				y = -h2;
-			else
-				visible++;
-
-			mLocationIsVisible = (visible == 2);
-
-			// set location indicator position
-			v.fromScreenPoint(x, y, mIndicatorPosition);
-		}
-
-		@Override
-		public void render(GLViewport v) {
-			GLState.useProgram(mShaderProgram);
-			GLState.blend(true);
-			GLState.test(false, false);
-
-			GLState.enableVertexArrays(hVertexPosition, -1);
-			MapRenderer.bindQuadVertexVBO(hVertexPosition);
-
-			float radius = POINTER_SIZE;
-
-			animate(true);
-			boolean viewShed = false;
-			if (!mLocationIsVisible /* || pos.zoomLevel < SHOW_ACCURACY_ZOOM */) {
-				//animate(true);
-			} else {
-				float r = (float) (mRadius * v.pos.scale);
+            animate(true);
+            boolean viewShed = false;
+            if (!mLocationIsVisible /* || pos.zoomLevel < SHOW_ACCURACY_ZOOM */) {
+                //animate(true);
+            } else {
+                float r = (float) (mRadius * v.pos.scale);
                 //if (r > radius) // || v.pos.zoomLevel >= SHOW_ACCURACY_ZOOM)
                 //    radius = r;
-				viewShed = true;
-				//animate(false);
-			}
-			gl.uniform1f(hScale, radius);
+                viewShed = true;
+                //animate(false);
+            }
+            gl.uniform1f(hScale, radius);
 
             double x = mIndicatorPosition.x - v.pos.x;
             double y = mIndicatorPosition.y - v.pos.y;
-			double tileScale = Tile.SIZE * v.pos.scale;
+            double tileScale = Tile.SIZE * v.pos.scale;
 
-			v.mvp.setTransScale((float) (x * tileScale), (float) (y * tileScale), 1);
-			v.mvp.multiplyMM(v.viewproj, v.mvp);
-			v.mvp.setAsUniform(hMatrixPosition);
+            v.mvp.setTransScale((float) (x * tileScale), (float) (y * tileScale), 1);
+            v.mvp.multiplyMM(v.viewproj, v.mvp);
+            v.mvp.setAsUniform(hMatrixPosition);
 
-			if (!viewShed) {
-				float phase = Math.abs(animPhase() - 0.5f) * 2;
-				//phase = Interpolation.fade.apply(phase);
-				phase = Interpolation.swing.apply(phase);
-
+            if (!viewShed) {
+                float phase = Math.abs(animPhase() - 0.5f) * 2;
+                phase = Interpolation.swing.apply(phase);
                 gl.uniform1f(hPhase, 0.8f + phase * 0.2f);
-			} else {
+            } else {
                 gl.uniform1f(hPhase, 1);
-			}
+            }
 
-			if (viewShed && mLocationIsVisible) {
-				float rotation = mBearing - 90;
+            if (viewShed && mLocationIsVisible) {
+                float rotation = mBearing - 90;
                 if (rotation > 180)
                     rotation -= 360;
                 else if (rotation < -180)
                     rotation += 360;
 
-				gl.uniform2f(hDirection,
+                gl.uniform2f(hDirection,
                         (float) Math.cos(Math.toRadians(rotation)),
                         (float) Math.sin(Math.toRadians(rotation)));
-			} else {
+            } else {
                 gl.uniform2f(hDirection, 0, 0);
-			}
+            }
 
-			// Pointer type
-			gl.uniform1f(hType, 0);
+            // Pointer type
+            gl.uniform1f(hType, 0);
 
-			gl.drawArrays(GL.TRIANGLE_STRIP, 0, 4);
-		}
+            gl.drawArrays(GL.TRIANGLE_STRIP, 0, 4);
+        }
 
-		private boolean init() {
-			int shader = GLShader.loadShader("location_pointer");
-			if (shader == 0)
-				return false;
+        private boolean init() {
+            int shader = GLShader.loadShader("location_pointer");
+            if (shader == 0)
+                return false;
 
-			mShaderProgram = shader;
-			hVertexPosition = gl.getAttribLocation(shader, "a_pos");
-			hMatrixPosition = gl.getUniformLocation(shader, "u_mvp");
-			hPhase = gl.getUniformLocation(shader, "u_phase");
-			hScale = gl.getUniformLocation(shader, "u_scale");
-			hDirection = gl.getUniformLocation(shader, "u_dir");
-			hType = gl.getUniformLocation(shader, "u_type");
+            mShaderProgram = shader;
+            hVertexPosition = gl.getAttribLocation(shader, "a_pos");
+            hMatrixPosition = gl.getUniformLocation(shader, "u_mvp");
+            hPhase = gl.getUniformLocation(shader, "u_phase");
+            hScale = gl.getUniformLocation(shader, "u_scale");
+            hDirection = gl.getUniformLocation(shader, "u_dir");
+            hType = gl.getUniformLocation(shader, "u_type");
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
