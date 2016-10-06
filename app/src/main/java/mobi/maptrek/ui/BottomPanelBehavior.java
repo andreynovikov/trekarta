33e106cc32de23ca8d2178410425739473287ad4
Andrey Novikov <novikov@gmail.com>
2016-10-06 19:26:59 +0300

    Snackbar behavior compacts button bar in landscape mode, closes #38

--- app/src/main/java/mobi/maptrek/ui/BottomPanelBehavior.java
+++ app/src/main/java/mobi/maptrek/ui/BottomPanelBehavior.java
@@ -9,10 +9,15 @@ import android.widget.RelativeLayout;
 
 import java.util.List;
 
+import mobi.maptrek.R;
+
 // It is referenced in layout file
 @SuppressWarnings("unused")
 public class BottomPanelBehavior extends CoordinatorLayout.Behavior<RelativeLayout> {
+    private final boolean mIsLandscape;
+
     public BottomPanelBehavior(Context context, AttributeSet attrs) {
+        mIsLandscape = context.getResources().getBoolean(R.bool.isLandscape);
     }
 
     @Override
@@ -23,15 +28,24 @@ public class BottomPanelBehavior extends CoordinatorLayout.Behavior<RelativeLayo
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, final RelativeLayout child, View dependency) {
         if (dependency instanceof Snackbar.SnackbarLayout) {
-            float translationY = 0;
+            float width = 0, shift = 0;
             final List<View> dependencies = parent.getDependencies(child);
             for (int i = 0, z = dependencies.size(); i < z; i++) {
                 final View view = dependencies.get(i);
                 if (view instanceof Snackbar.SnackbarLayout && parent.doViewsOverlap(child, view)) {
-                    translationY = Math.min(translationY, view.getTranslationY() - view.getHeight());
+                    shift = Math.min(shift, view.getTranslationY() - view.getHeight());
+                    width = parent.getWidth() - view.getWidth();
                 }
             }
-            child.setTranslationY(translationY);
+            if (mIsLandscape) {
+                if (width < 10f) {
+                    child.setPaddingRelative(0, 0, 0, (int) -shift);
+                    return true;
+                }
+            } else {
+                child.setTranslationY(shift);
+                return true;
+            }
         }
         return false;
     }
