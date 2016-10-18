79dba434c972d04bf567ab4ebb57f821311b441d
Andrey Novikov <novikov@gmail.com>
2016-10-18 14:11:47 +0300

    Import files through old view intent

--- app/src/main/java/mobi/maptrek/DataImportActivity.java
+++ app/src/main/java/mobi/maptrek/DataImportActivity.java
@@ -1,5 +1,6 @@
 package mobi.maptrek;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
@@ -8,6 +9,7 @@ import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,6 +18,7 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
 import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DateFormat;
@@ -46,6 +50,7 @@ public class DataImportActivity extends Activity {
     private static final Logger logger = LoggerFactory.getLogger(DataImportActivity.class);
     private static final String DATA_IMPORT_FRAGMENT = "dataImportFragment";
     private static final DateFormat SUFFIX_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
+    private static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
 
     private DataImportFragment mDataImportFragment;
     private ImportProgressHandler mProgressHandler;
@@ -53,6 +58,8 @@ public class DataImportActivity extends Activity {
     private TextView mFileNameView;
     private ProgressBar mProgressBar;
     private Button mActionButton;
+
+    private Runnable mTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,6 +124,42 @@ public class DataImportActivity extends Activity {
         mProgressBar.setProgress(savedInstanceState.getInt("progressBarProgress"));
         mActionButton.setText(savedInstanceState.getString("action"));
         mActionButton.setTag(savedInstanceState.getBoolean("finished"));
+    }
+
+    private void askForPermission(Runnable task) {
+        mTask = task;
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+
+        } else {
+            mDataImportFragment.startImport(mTask);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (mTask != null)
+                        mDataImportFragment.startImport(mTask);
+                } else {
+                    finish();
+                }
+                // return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @SuppressLint("HandlerLeak")
@@ -199,64 +242,65 @@ public class DataImportActivity extends Activity {
         }
 
         private void processIntent(final Intent intent) {
-            // It appears to be fast enough but I will live it in separate background thread
-            final Message m = Message.obtain(mBackgroundHandler, new Runnable() {
-                @Override
-                public void run() {
-                    String action = intent.getAction();
-                    String type = intent.getType();
-                    logger.debug("Action: {}", action);
-                    logger.debug("Type: {}", type);
-
-                    if (Intent.ACTION_SEND.equals(action) || Intent.ACTION_VIEW.equals(action)) {
-                        handleSendOrView(intent);
-                    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action)) {
-                        handleSendMultiple(intent);
+            String action = intent.getAction();
+            String type = intent.getType();
+            logger.debug("Action: {}", action);
+            logger.debug("Type: {}", type);
+            if (Intent.ACTION_SEND.equals(action) || Intent.ACTION_VIEW.equals(action)) {
+                Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (uri == null)
+                    uri = intent.getData();
+                logger.debug("Uri: {}", uri.toString());
+                final Uri finalUri = uri;
+                Runnable task = new Runnable() {
+                    @Override
+                    public void run() {
+                        readFile(finalUri);
                     }
+                };
+                String scheme = uri.getScheme();
+                if ("file".equals(scheme)) {
+                    ((DataImportActivity) getActivity()).askForPermission(task);
+                } else {
+                    startImport(task);
                 }
-            });
-            mBackgroundHandler.sendMessage(m);
-        }
-
-        private void handleSendOrView(Intent intent) {
-            Uri streamUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            if (streamUri != null) {
-                readFile(streamUri);
-            } else {
-                readFile(intent.getData());
-            }
-        }
-
-        private void handleSendMultiple(Intent intent) {
-            ArrayList<Uri> uris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-            //FIXME Not tested at all!
-            if (uris != null) {
-                for (Uri uri : uris)
-                    readFile(uri);
+            } else if (Intent.ACTION_SEND_MULTIPLE.equals(action)) {
+                startImport(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArrayList<Uri> uris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                        //FIXME Not tested at all!
+                        if (uris != null) {
+                            for (Uri uri : uris)
+                                readFile(uri);
+                        }
+                    }
+                });
             }
         }
 
         private void readFile(Uri uri) {
             Context context = getContext();
-            logger.debug(uri.toString());
+
+            String name = null;
+            long length = -1;
+
             String scheme = uri.getScheme();
             if ("file".equals(scheme)) {
                 String path = uri.getPath();
                 File src = new File(path);
-                String name = uri.getLastPathSegment();
-                long size = src.length();
-                logger.debug("file: {} [{}]", name, size);
-                File dst = getDestinationFile(name);
+                name = uri.getLastPathSegment();
+                length = src.length();
                 try {
-                    FileUtils.copyFile(src, dst);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    mInputStream = new MonitoredInputStream(new FileInputStream(src));
+                } catch (FileNotFoundException e) {
+                    logger.error("Failed to get imported file stream", e);
+                    return;
                 }
+                logger.debug("file: {} [{}]", name, length);
             } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
                 ContentResolver resolver = context.getContentResolver();
                 Cursor cursor = resolver.query(uri, new String[]{OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE}, null, null, null);
-                String name = null;
-                long length = -1;
                 if (cursor != null) {
                     logger.debug("   from cursor");
                     cursor.moveToFirst();
@@ -271,48 +315,53 @@ public class DataImportActivity extends Activity {
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
-                    logger.debug("Length: " + length);
                 }
                 //TODO This is just to not crash, should be correctly implemented
                 if (name == null)
                     name = uri.getLastPathSegment();
 
-                logger.debug("Import: [{}][{}]", name, length);
-
-                if (!name.endsWith(TrackManager.EXTENSION) &&
-                        !name.endsWith(KMLManager.EXTENSION) &&
-                        !name.endsWith(GPXManager.EXTENSION)) {
-                    logger.warn("Unsupported file format");
+                try {
+                    mInputStream = new MonitoredInputStream(resolver.openInputStream(uri));
+                } catch (FileNotFoundException e) {
+                    logger.error("Failed to get imported file stream", e);
                     return;
                 }
-
-                mProgressListener.onProgressStarted((int) length);
-                mProgressListener.onProgressAnnotated(name);
-                File dst = null;
-                try {
-                    dst = getDestinationFile(name);
-
-                    mInputStream = new MonitoredInputStream(resolver.openInputStream(uri));
-                    mInputStream.addChangeListener(new MonitoredInputStream.ChangeListener() {
-                        @Override
-                        public void stateChanged(long location) {
-                            if (mProgressListener != null) {
-                                //TODO Divide progress by 1024
-                                mProgressListener.onProgressChanged((int) location);
-                            }
-                        }
-                    });
-
-                    FileUtils.copyStreamToFile(mInputStream, dst);
-                    mProgressListener.onProgressFinished();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    if (dst != null && dst.exists())
-                        //noinspection ResultOfMethodCallIgnored
-                        dst.delete();
-                }
+                logger.debug("Import: [{}][{}]", name, length);
             } else {
                 logger.warn("Unsupported transfer method");
+                return;
+            }
+
+            if (!name.endsWith(TrackManager.EXTENSION) &&
+                    !name.endsWith(KMLManager.EXTENSION) &&
+                    !name.endsWith(GPXManager.EXTENSION)) {
+                logger.warn("Unsupported file format");
+                return;
+            }
+
+            mProgressListener.onProgressStarted((int) length);
+            mProgressListener.onProgressAnnotated(name);
+            File dst = null;
+            try {
+                dst = getDestinationFile(name);
+
+                mInputStream.addChangeListener(new MonitoredInputStream.ChangeListener() {
+                    @Override
+                    public void stateChanged(long location) {
+                        if (mProgressListener != null) {
+                            //TODO Divide progress by 1024
+                            mProgressListener.onProgressChanged((int) location);
+                        }
+                    }
+                });
+
+                FileUtils.copyStreamToFile(mInputStream, dst);
+                mProgressListener.onProgressFinished();
+            } catch (IOException e) {
+                e.printStackTrace();
+                if (dst != null && dst.exists())
+                    //noinspection ResultOfMethodCallIgnored
+                    dst.delete();
             }
         }
 
@@ -330,6 +379,11 @@ public class DataImportActivity extends Activity {
                 destination = new File(dir, filename);
             }
             return destination;
+        }
+
+        public void startImport(Runnable task) {
+            final Message m = Message.obtain(mBackgroundHandler, task);
+            mBackgroundHandler.sendMessage(m);
         }
 
         public void stopImport() {
