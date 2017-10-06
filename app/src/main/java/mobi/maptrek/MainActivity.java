ee084de7805c1d00b1428e7bcbc74c3a07d0ee82
Andrey Novikov <novikov@gmail.com>
2017-10-06 15:42:19 +0300

    Disable FTS for lite version

--- app/src/main/java/mobi/maptrek/MainActivity.java
+++ app/src/main/java/mobi/maptrek/MainActivity.java
@@ -1601,6 +1601,7 @@ public class MainActivity extends BasePluginActivity implements ILocationListene
                         menu.findItem(R.id.actionHideSystemUI).setChecked(Configuration.getHideSystemUI());
                     } else {
                         menu.removeItem(R.id.actionHideSystemUI);
+                        menu.removeItem(R.id.actionSearch);
                     }
                     if (mGaugePanel.hasVisibleGauges() || (mLocationState != LocationState.NORTH && mLocationState != LocationState.TRACK))
                         menu.removeItem(R.id.actionAddGauge);
