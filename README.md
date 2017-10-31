eb97bab30c1bf7f54967ee5a846806d8dc0fdea0
Andrey Novikov <novikov@gmail.com>
2017-10-31 22:31:37 +0300

    Use custom color picker library

--- README.md
+++ README.md
@@ -16,13 +16,15 @@ The development is in beta stage now. Bug reports and feature proposals are welc
 2. Clone maptrek branch of vtm repository
 3. Run the following graddle tasks (these will install required vtm snapshots in local Maven repository):
 
-    vtm:vtm imstall
+    vtm:vtm install
     vtm:vtm-android install
     vtm:vtm-android install
 
-4. Clone maptrek submodules
+4. Clone androidcolorpicker repository
+5. Run install task
+6. Clone maptrek submodules
 
     git submodule update --init --recursive
 
-5. Build maptrek project
+7. Build maptrek project
 
--- app/src/main/java/mobi/maptrek/data/style/MarkerStyle.java
+++ app/src/main/java/mobi/maptrek/data/style/MarkerStyle.java
@@ -1,6 +1,25 @@
 package mobi.maptrek.data.style;
 
 public class MarkerStyle extends Style<MarkerStyle> {
+    public static final int[] DEFAULT_COLORS = {
+            android.graphics.Color.DKGRAY,
+            0xff2062af,
+            0xff58AEB7,
+            0xffF4B528,
+            0xffDD3E48,
+            0xffBF89AE,
+            0xff5C88BE,
+            0xff59BC10,
+            0xffE87034,
+            0xfff84c44,
+            0xff8c47fb,
+            0xff51C1EE,
+            0xff8cc453,
+            0xffC2987D,
+            0xffCE7777,
+            0xff9086BA
+    };
+
     public static int DEFAULT_COLOR = android.graphics.Color.DKGRAY;
 
     public int color = DEFAULT_COLOR;
