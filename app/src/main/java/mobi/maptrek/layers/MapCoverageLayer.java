81367f7a2d263e63613944abc6caeea3b9cd7249
Andrey Novikov <novikov@gmail.com>
2017-11-20 19:12:13 +0300

    Visualize hillshades downloads

--- app/src/main/java/mobi/maptrek/layers/MapCoverageLayer.java
+++ app/src/main/java/mobi/maptrek/layers/MapCoverageLayer.java
@@ -1,9 +1,15 @@
 package mobi.maptrek.layers;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.text.format.DateFormat;
 import android.text.format.Formatter;
 
+import org.oscim.android.canvas.AndroidBitmap;
+import org.oscim.android.canvas.AndroidGraphics;
+import org.oscim.backend.CanvasAdapter;
+import org.oscim.backend.canvas.Bitmap;
 import org.oscim.backend.canvas.Color;
 import org.oscim.backend.canvas.Paint;
 import org.oscim.core.Box;
@@ -20,12 +26,17 @@ import org.oscim.layers.vector.AbstractVectorLayer;
 import org.oscim.map.Map;
 import org.oscim.renderer.bucket.LineBucket;
 import org.oscim.renderer.bucket.MeshBucket;
+import org.oscim.renderer.bucket.SymbolBucket;
+import org.oscim.renderer.bucket.SymbolItem;
 import org.oscim.renderer.bucket.TextBucket;
 import org.oscim.renderer.bucket.TextItem;
 import org.oscim.theme.styles.AreaStyle;
 import org.oscim.theme.styles.LineStyle;
 import org.oscim.theme.styles.TextStyle;
+import org.oscim.utils.ColorUtil;
 import org.oscim.utils.FastMath;
+
+import java.io.IOException;
 
 import mobi.maptrek.BuildConfig;
 import mobi.maptrek.Configuration;
@@ -37,8 +48,19 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
     private static final long MAP_EXPIRE_PERIOD = 6; // one week
     private static final int FADE_ZOOM = 3;
     private static final double MIN_SCALE = 10d;
+    private static final double SYMBOL_MIN_SCALE = 30d;
     public static final double TEXT_MIN_SCALE = 40d;
     private static final double TEXT_MAX_SCALE = 260d;
+    private static final float AREA_ALPHA = 0.7f;
+
+    private static final int COLOR_ACTIVE = ColorUtil.modHsv(Color.GREEN, 1.0, 0.7, 0.8, false);
+    private static final int COLOR_DOWNLOADING = ColorUtil.modHsv(COLOR_ACTIVE, 1.0, 1.0, 0.7, false);
+    private static final int COLOR_OUTDATED = ColorUtil.modHsv(Color.YELLOW, 1.0, 0.6, 0.8, false);
+    private static final int COLOR_MISSING = ColorUtil.modHsv(Color.GRAY, 1.0, 1.0, 1.1, false);
+    private static final int COLOR_SELECTED = ColorUtil.modHsv(Color.BLUE, 1.0, 0.7, 0.8, false);
+    private static final int COLOR_DELETED = ColorUtil.modHsv(Color.RED, 1.0, 0.7, 0.8, false);
+    private static final int COLOR_TEXT = Color.get(0, 96, 0);
+    private static final int COLOR_TEXT_OUTLINE = Color.get(224, 224, 224);
 
     private final Index mMapIndex;
     private boolean mAccountHillshades;
@@ -51,23 +73,27 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
     private final AreaStyle mDeletedAreaStyle;
     private final LineStyle mLineStyle;
     private final TextStyle mTextStyle;
-    private final java.text.DateFormat mDateFormat;
     private final TextStyle mSmallTextStyle;
+    private final Bitmap mHillshadesBitmap;
+    private final Bitmap mPresentHillshadesBitmap;
+    private final java.text.DateFormat mDateFormat;
     private Context mContext;
 
     public MapCoverageLayer(Context context, Map map, Index mapIndex, float scale) {
         super(map);
         mContext = context;
         mMapIndex = mapIndex;
-        mPresentAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.GREEN).blendScale(10).color(Color.fade(Color.GREEN, 0.4f)).build();
-        mOutdatedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.YELLOW).blendScale(10).color(Color.fade(Color.YELLOW, 0.4f)).build();
-        mMissingAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.GRAY).blendScale(10).color(Color.fade(Color.GRAY, 0.4f)).build();
-        mDownloadingAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.GREEN & Color.GRAY).blendScale(10).color(Color.fade(Color.GREEN & Color.GRAY, 0.4f)).build();
-        mSelectedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.BLUE).blendScale(10).color(Color.fade(Color.BLUE, 0.4f)).build();
-        mDeletedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(Color.RED).blendScale(10).color(Color.fade(Color.RED, 0.4f)).build();
+        mPresentAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_ACTIVE).blendScale(10).color(Color.fade(COLOR_ACTIVE, AREA_ALPHA)).build();
+        mOutdatedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_OUTDATED).blendScale(10).color(Color.fade(COLOR_OUTDATED, AREA_ALPHA)).build();
+        mMissingAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_MISSING).blendScale(10).color(Color.fade(COLOR_MISSING, AREA_ALPHA)).build();
+        mDownloadingAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_DOWNLOADING).blendScale(10).color(Color.fade(COLOR_DOWNLOADING, AREA_ALPHA)).build();
+        mSelectedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_SELECTED).blendScale(10).color(Color.fade(COLOR_SELECTED, AREA_ALPHA)).build();
+        mDeletedAreaStyle = AreaStyle.builder().fadeScale(FADE_ZOOM).blendColor(COLOR_DELETED).blendScale(10).color(Color.fade(COLOR_DELETED, AREA_ALPHA)).build();
         mLineStyle = LineStyle.builder().fadeScale(FADE_ZOOM + 1).color(Color.fade(Color.DKGRAY, 0.6f)).strokeWidth(0.5f * scale).fixed(true).build();
-        mTextStyle = TextStyle.builder().fontSize(10 * scale).fontStyle(Paint.FontStyle.BOLD).color(Color.get(0, 64, 0)).strokeColor(Color.WHITE).strokeWidth(8f).build();
-        mSmallTextStyle = TextStyle.builder().fontSize(8 * scale).fontStyle(Paint.FontStyle.BOLD).color(Color.get(0, 64, 0)).strokeColor(Color.WHITE).strokeWidth(8f).build();
+        mTextStyle = TextStyle.builder().fontSize(11 * scale).fontStyle(Paint.FontStyle.BOLD).color(COLOR_TEXT).strokeColor(COLOR_TEXT_OUTLINE).strokeWidth(7f).build();
+        mSmallTextStyle = TextStyle.builder().fontSize(8 * scale).fontStyle(Paint.FontStyle.BOLD).color(COLOR_TEXT).strokeColor(COLOR_TEXT_OUTLINE).strokeWidth(5f).build();
+        mHillshadesBitmap = getHillshadesBitmap(Color.fade(Color.DKGRAY, 0.8f));
+        mPresentHillshadesBitmap = getHillshadesBitmap(Color.WHITE);
         mDateFormat = DateFormat.getDateFormat(context);
         mMapIndex.addMapStateListener(this);
         mAccountHillshades = Configuration.getHillshadesEnabled();
@@ -77,6 +103,8 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
     public void onDetach() {
         super.onDetach();
         mMapIndex.removeMapStateListener(this);
+        mHillshadesBitmap.recycle();
+        mPresentHillshadesBitmap.recycle();
     }
 
     @Override
@@ -90,6 +118,7 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
             return;
 
         float scale = (float) (t.position.scale * Tile.SIZE / UNSCALE_COORD);
+        float pxScale = (float) (t.position.scale / UNSCALE_COORD);
 
         int tileXMin = (int) (MercatorProjection.longitudeToX(b.xmin) / TILE_SCALE);
         int tileXMax = (int) (MercatorProjection.longitudeToX(b.xmax) / TILE_SCALE);
@@ -113,7 +142,9 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
 
             TextBucket text = null;
             if (t.position.scale >= TEXT_MIN_SCALE && t.position.scale <= TEXT_MAX_SCALE)
-                text = t.buckets.getTextBucket(-1);
+                text = t.buckets.getTextBucket(7);
+
+            SymbolBucket symbols = new SymbolBucket();
 
             for (int tileX = tileXMin; tileX <= tileXMax; tileX++) {
                 for (int tileY = tileYMin; tileY <= tileYMax; tileY++) {
@@ -169,6 +200,22 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
                     y -= TILE_SCALE;
                     lines.addPoint(x * scale, y * scale);
 
+                    if (t.position.scale >= SYMBOL_MIN_SCALE && t.position.scale <= TEXT_MAX_SCALE
+                            && mHillshadesBitmap != null
+                            && mPresentHillshadesBitmap != null) {
+                        SymbolItem s = new SymbolItem();
+                        if (mapStatus.hillshadeVersion > 0)
+                            s.bitmap = mPresentHillshadesBitmap;
+                        else if (mAccountHillshades && mapStatus.hillshadeDownloadSize > 0L)
+                            s.bitmap = mHillshadesBitmap;
+                        if (s.bitmap != null) {
+                            s.x = (x + TILE_SCALE) * scale - s.bitmap.getWidth() * pxScale * TILE_SCALE * 2f;
+                            s.y = y * scale + s.bitmap.getHeight() * pxScale * TILE_SCALE * 1.7f;
+                            s.billboard = false;
+                            symbols.addSymbol(s);
+                        }
+                    }
+
                     if (text != null) {
                         float tx = (x + TILE_SCALE / 2) * scale;
                         float ty = (y + TILE_SCALE / 2) * scale;
@@ -201,8 +248,6 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
             if (line.line == null)
                 line.line = mLineStyle;
             line.addLine(lines);
-            if (text != null)
-                text.next = line;
 
             MeshBucket missing = t.buckets.getMeshBucket(1);
             if (missing.area == null)
@@ -240,8 +285,10 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
             downloading.addMesh(downloadingAreas);
             deleted.next = downloading;
 
+            downloading.next = symbols;
+
             if (text != null)
-                text.setLevel(7);
+                symbols.next = text;
         }
     }
 
@@ -293,5 +340,21 @@ public class MapCoverageLayer extends AbstractVectorLayer<MapFile> implements Ge
     @Override
     public void onMapSelected(int x, int y, Index.ACTION action, Index.IndexStats stats) {
         update();
+    }
+
+    private static Bitmap getHillshadesBitmap(int color) {
+        Bitmap bitmap;
+        try {
+            bitmap = CanvasAdapter.getBitmapAsset("", "symbols/hillshades.svg", 0, 0, 70);
+        } catch (IOException e) {
+            log.error("Failed to read bitmap", e);
+            return null;
+        }
+        android.graphics.Bitmap bitmapResult = android.graphics.Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), android.graphics.Bitmap.Config.ARGB_8888);
+        android.graphics.Canvas canvas = new android.graphics.Canvas(bitmapResult);
+        android.graphics.Paint paint = new android.graphics.Paint();
+        paint.setColorFilter(new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(AndroidGraphics.getBitmap(bitmap), 0, 0, paint);
+        return new AndroidBitmap(bitmapResult);
     }
 }
