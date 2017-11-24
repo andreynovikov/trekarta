c083f2a107e8ffabaf39c3f2afc13e91fdd2a0f2
Andrey Novikov <novikov@gmail.com>
2017-11-24 15:47:07 +0300

    Add activity introductions

--- app/src/main/java/mobi/maptrek/IntroductionActivity.java
+++ app/src/main/java/mobi/maptrek/IntroductionActivity.java
@@ -11,7 +11,7 @@ import com.github.paolorotolo.appintro.model.SliderPage;
 import mobi.maptrek.fragments.IntroductionFragment;
 
 public class IntroductionActivity extends AppIntro {
-    public static final int CURRENT_INTRODUCTION = 2;
+    public static final int CURRENT_INTRODUCTION = 3;
     int mLastSeenIntroduction;
 
     @Override
@@ -39,6 +39,19 @@ public class IntroductionActivity extends AppIntro {
             sliderPage.setImageDrawable(R.mipmap.sharepoint);
             addSlide(IntroductionFragment.newInstance(sliderPage));
             mLastSeenIntroduction = 2;
+        }
+
+        if (mLastSeenIntroduction < 3) {
+            sliderPage.setTitle("Hiking");
+            sliderPage.setDescription("Hiking activity mode emphasises paths and tracks. It visualizes path difficulty and visibility and shows hiking routes.");
+            sliderPage.setImageDrawable(R.mipmap.hiking);
+            addSlide(IntroductionFragment.newInstance(sliderPage));
+
+            sliderPage.setTitle("Skiing and skating");
+            sliderPage.setDescription("Skiing activity mode displays clean winter map with mostly all skiing activities: downhill, nordic, hiking and touring. As a bonus freestyle snow-boarding, skating and sleighing areas are displayed.");
+            sliderPage.setImageDrawable(R.mipmap.skiing);
+            addSlide(IntroductionFragment.newInstance(sliderPage));
+            mLastSeenIntroduction = 3;
         }
 
         // TODO Do not show more then N slides at once
