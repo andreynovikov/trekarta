ac896c86e8c6c2fd1189159c34c57271e772b892
Andrey Novikov <novikov@gmail.com>
2020-04-08 23:24:49 +0300

    Do not show What's New dialog after installation

--- app/src/main/java/mobi/maptrek/fragments/WhatsNewDialog.java
+++ app/src/main/java/mobi/maptrek/fragments/WhatsNewDialog.java
@@ -87,7 +87,7 @@ public class WhatsNewDialog extends DialogFragment {
     public void show(FragmentManager manager, String tag) {
         MapTrek application = MapTrek.getApplication();
         int lastCode = Configuration.getLastSeenChangelog();
-        if (lastCode < MapTrek.versionCode) {
+        if (lastCode > 0 && lastCode < MapTrek.versionCode) {
             getChangelog(application, lastCode);
             if (mChangelog.size() > 0)
                 super.show(manager, tag);
