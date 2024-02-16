f84605be11ec97c6ba9bcba2ca459eedf0704964
Andrey Novikov <novikov@gmail.com>
2024-02-17 00:02:27 +0300

    VTM: Add missing @Override annotations

--- app/src/main/java/org/oscim/tiling/source/bitmap/BitmapTileSource.java
+++ app/src/main/java/org/oscim/tiling/source/bitmap/BitmapTileSource.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2013 Hannes Janetzek
+ * Copyright 2018 devemux86
+ *
+ * This file is part of the OpenScienceMap project (http://www.opensciencemap.org).
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.oscim.tiling.source.bitmap;
 
 import org.oscim.backend.CanvasAdapter;
@@ -24,6 +41,7 @@ public class BitmapTileSource extends UrlTileSource {
             super(null, "/{Z}/{X}/{Y}.png", 0, 17);
         }
 
+        @Override
         public BitmapTileSource build() {
             return new BitmapTileSource(this);
         }
